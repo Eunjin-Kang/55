ba61317414fb37cc9d5a7a518d6e4040264b5c20
Kangeujin <rkd2289@naver.com>
2017-01-20 18:03:18 +0900

    fd

---
+++ app/src/main/java/com/example/b10718/a0119/Config.java
@@ -0,0 +1,27 @@
+package com.example.b10718.a0119;
+
+/**
+ * Created by B10718 on 2017-01-19.
+ */
+
+public class Config {
+    //URL to our login.php file
+    public static final String LOGIN_URL = "http://192.168.0.3/v1/login.php";
+
+    //Keys for email and password as defined in our $_POST['key'] in login.php
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+
+    //If server response is equal to this that means login is successful
+    public static final String LOGIN_SUCCESS = "success";
+
+    //Keys for Sharedpreferences
+    //This would be the name of our shared preferences
+    public static final String SHARED_PREF_NAME = "myloginapp";
+
+    //This would be used to store the email of current logged in user
+    public static final String EMAIL_SHARED_PREF = "email";
+
+    //We will use this to store the boolean in sharedpreference to track user is loggedin or not
+    public static final String LOGGEDIN_SHARED_PREF = "loggedin";
+}
