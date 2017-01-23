b514997475297c5a829e93b1703ab6664e6714cd
KeePet <tigerk9212@gmail.com>
2017-01-23 13:35:22 +0900

    12

--- app/src/main/java/com/example/b10718/a0119/fuckingeunjin.java
+++ app/src/main/java/com/example/b10718/a0119/fuckingeunjin.java
@@ -1,8 +1,16 @@
 package com.example.b10718.a0119;
 
+import android.app.ProgressDialog;
+import android.support.v7.widget.AppCompatButton;
+import android.widget.EditText;
+
 /**
  * Created by B10724 on 2017-01-20.
  */
 
 public class fuckingeunjin {
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private AppCompatButton buttonLogin;
+    private ProgressDialog progressDialog;
 }
